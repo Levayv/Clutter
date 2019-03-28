c4123c2dbdfce0bf3e7960cb5493368e8fc83c32
Levon Ayvazyan <levayv43@gmail.com>
2019-03-28 07:31:17 +0400

    Added Bishop AND Rook !

--- src/am/levayv/testing/claio/chess/model/piece/Bishop.java
+++ src/am/levayv/testing/claio/chess/model/piece/Bishop.java
@@ -1,4 +1,33 @@
 package am.levayv.testing.claio.chess.model.piece;
 
-public class Bishop {
+import am.levayv.testing.claio.chess.model.Cell;
+import am.levayv.testing.claio.chess.model.Navigator;
+import am.levayv.testing.claio.chess.model.piece.data.Color;
+import am.levayv.testing.claio.chess.model.piece.data.PieceType;
+
+import java.util.HashSet;
+
+public class Bishop extends Piece {
+    public Bishop(Color color) {
+        super(color);
+    }
+
+    @Override
+    protected PieceType initType() {
+        return PieceType.BISHOP;
+    }
+
+    @Override
+    protected void updateAvailableMoves(Cell current , HashSet<Cell> set) {
+        //todo IMF - Queen move unlmited cells in any direction , total of 8 directions max 27 !
+        for (Navigator.Dir dir:
+                Navigator.Dir.values()) {
+
+            // ignore diagonal moves 0123 ~ 4567
+            if (dir.ordinal()>=4) //todo refactor Dir enum
+                this.baseTraversal(current,dir);
+        }
+    }
+
+
 }
--- src/am/levayv/testing/claio/chess/model/piece/Knight.java
+++ src/am/levayv/testing/claio/chess/model/piece/Knight.java
@@ -1,4 +1,32 @@
 package am.levayv.testing.claio.chess.model.piece;
 
-public class Knight {
+import am.levayv.testing.claio.chess.model.Cell;
+import am.levayv.testing.claio.chess.model.Navigator;
+import am.levayv.testing.claio.chess.model.piece.data.Color;
+import am.levayv.testing.claio.chess.model.piece.data.PieceType;
+
+import java.util.HashSet;
+
+public class Knight extends Piece {
+    public Knight(Color color) {
+        super(color);
+    }
+
+    @Override
+    protected PieceType initType() {
+        return PieceType.KNIGHT;
+    }
+
+    @Override
+    protected void updateAvailableMoves(Cell current , HashSet<Cell> set) {
+        //todo IMF - Queen move unlmited cells in any direction , total of 8 directions max 27 !
+        for (Navigator.Dir dir:
+                Navigator.Dir.values()) {
+
+            // todo IMF static 8 fields no traversal
+
+        }
+    }
+
+
 }
